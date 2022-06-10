77458f06cb9838bd28be0d66526883fa3887b39e
yamazak-itakaaki <ksw2170013@stu.o-hara.ac.jp>
2022-06-10 12:34:40 +0900

    e

--- testPj/testPj/settings.py
+++ testPj/testPj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'testPj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': hostgres / 'db.postgresql',
+        'USER'postgres:
+        'PASSWORD'70013:
+        'HOST'localhost:
+        'POST': 5432,
     }
 }
 
