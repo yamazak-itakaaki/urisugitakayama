2b999284082a7de8d727b6c60f43bcea1a2e51e3
urita-fuki <nogihina0809@i.softbank.jp>
2022-06-14 12:06:15 +0900

    削除6/14

--- testPj/hinatazaka/urls.py
+++ testPj/hinatazaka/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from  .views import hinatazakaListView
 from .views import hinatazakaProfileView
+from .views import  hinatazakaDeleteView
 urlpatterns = [
     path("",hinatazakaListView),
     path("profile/<int:id>/",hinatazakaProfileView,name="hinatazaka-profile"),
+    path("delete/<int:pk>/", hinatazakaDeleteView, name="hinatazaka-delete"),
 ]
--- testPj/hinatazaka/views.py
+++ testPj/hinatazaka/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Hinatazaka46
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def hinatazakaListView(request):
     template_name = "hinatazaka-list.html"
@@ -26,3 +27,16 @@ def nippoCreateView(request):
     
     return render(request, template_name)
 
+def nippoDeleteView(request, pk):
+    template_name = "hinatazaka-delete.html"
+    obj = get_object_or_404(Hinatazaka46, pk=pk)
+    ctx = {"object": obj}
+    return render(request, template_name, ctx)
+
+def nippoDeleteView(request, pk):
+    template_name = "hinatazaka-delete.html"
+    obj = get_object_or_404(Hinatazaka46, pk=pk)
+    ctx = {"object": obj}
+    if request.POST:
+        obj.delete()
+    return render(request, template_name, ctx)
