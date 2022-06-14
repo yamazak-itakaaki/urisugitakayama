acd90ee3b072b69b5bbcd5dc7b817c307da77f2d
yamazak-itakaaki <ksw2170013@stu.o-hara.ac.jp>
2022-06-14 12:18:38 +0900

    戻るようにした

--- testPj/hinatazaka/urls.py
+++ testPj/hinatazaka/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from  .views import hinatazakaListView
 from .views import hinatazakaProfileView
+from .views import hinatazakaCreateView
 urlpatterns = [
-    path("",hinatazakaListView),
+    path("",hinatazakaListView, name='hinatazaka-list'),
     path("profile/<int:id>/",hinatazakaProfileView,name="hinatazaka-profile"),
+    path("form/",hinatazakaCreateView,name="hinatazaka-create"),
 ]
