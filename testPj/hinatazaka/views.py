442392738ed60c41c83e69e09eb4ef3784d22435
yamazak-itakaaki <ksw2170013@stu.o-hara.ac.jp>
2022-06-14 11:55:55 +0900

    変数変更

--- testPj/hinatazaka/views.py
+++ testPj/hinatazaka/views.py
@@ -16,7 +16,7 @@ def hinatazakaProfileView(request,id):
 
     return render(request, template_name,ctx)
 
-def nippoCreateView(request):
+def hinatazakaCreateView(request):
     template_name="hinatazaka-form.html"
     if request.POST:
         miyozi = request.POST["miyozi"]
