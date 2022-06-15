ce17330cc2e14ece1e4b39ee95a4f294c31d9dcc
yamazak-itakaaki <ksw2170013@stu.o-hara.ac.jp>
2022-06-15 10:45:18 +0900

    更新ver2キャンセル

--- testPj/hinatazaka/urls.py
+++ testPj/hinatazaka/urls.py
@@ -3,15 +3,14 @@ from  .views import hinatazakaListView
 from .views import hinatazakaProfileView
 from .views import hinatazakaCreateView
 from .views import hinatazakaDeleteView
-from .views import hinatazakaEditFormView
+from .views import hinatazakaUpdateFormView
 
 urlpatterns = [
     path("",hinatazakaListView, name='hinatazaka-list'),
     path("profile/<int:id>/",hinatazakaProfileView,name="hinatazaka-profile"),
     path("delete/<int:id>/", hinatazakaDeleteView, name="hinatazaka-delete"),
     path("form/",hinatazakaCreateView,name="hinatazaka-create"),
-    path("edit/<int:id>/", hinatazakaEditFormView, name="hinatazaka-edit"),
-    path('update/<int:id>/' hinatazakaUpdateFormView, name = "hinatazaka-update"),
+    path('update/<int:id>/', hinatazakaUpdateFormView, name = "hinatazaka-update"),
     path("delete/<int:id>/", hinatazakaDeleteView, name="hinatazaka-delete"),
     
 
--- testPj/hinatazaka/views.py
+++ testPj/hinatazaka/views.py
@@ -46,59 +46,26 @@ def hinatazakaDeleteView(request, id):
         return redirect("hinatazaka-list")
     return render(request, template_name, ctx)
 
-    from .forms import  NippoFormClass
+    from .forms import  HinatazakaFormClass
 
 
-# def hinatazakaUpdateFormView(request, id):
-#     template_name = "hinatazaka-form.html"
-#     obj = Hinatazaka46.objects.get(id=id)
-#     initial_values = {"miyozi":obj.miyozi,"name":obj.name,"birthday":obj.birthday,"city":obj.city}
-#     form = HinatazakaFormClass(request.POST or initial_values)
-#     ctx = {"form": form}
-#     if form.is_valid():
-#         miyozi = form.cleaned_data["miyozi"]
-#         name = form.cleaned_data["name"]
-#         birthday = form.cleaned_data["birthday"]
-#         city = form.cleaned_data["city"]
-#         obj.miyozi = miyozi
-#         obj.name = name
-#         obj.birthday = birthday
-#         obj.city = city
-#         obj.save()
-#         return redirect('hinatazaka-list')
-#     return render(request, template_name, ctx) 
-
-def hinatazakaEditFormView(request, id):
-    template_name = "formclass.html"
-    #idをもとにユーザ情報を取得
+def hinatazakaUpdateFormView(request, id):
+    template_name = "hinatazaka-form.html"
     obj = Hinatazaka46.objects.get(id=id)
-    #フォームをオブジェクトを作成
-    userForm = UserForm(instance=obj)
-    
-    #ユーザ情報をフォームに格納
-    context = {
-        'obj':obj,
-        'userForm':userForm,
-    }
- 
-    #user.htmlへデータを渡す
-    return render(request, template_name, context)
+    initial_values = {"miyozi":obj.miyozi,"name":obj.name,"birthday":obj.birthday,"city":obj.city}
+    form = HinatazakaFormClass(request.POST or initial_values)
+    ctx = {"form": form}
+    if form.is_valid():
+        miyozi = form.cleaned_data["miyozi"]
+        name = form.cleaned_data["name"]
+        birthday = form.cleaned_data["birthday"]
+        city = form.cleaned_data["city"]
+        obj.miyozi = miyozi
+        obj.name = name
+        obj.birthday = birthday
+        obj.city = city
+        obj.save()
+        return redirect('hinatazaka-list')
+    return render(request, template_name, ctx) 
 
-def hinatazakaUpdateFormView(request,id):
-    #リクエストがPOSTの場合
-    if request.method == 'POST':
-        #idからユーザデータを取得
-        obj = Hinatazaka46.objects.get(id=id)
-        #ユーザ情報と、リクエストをもとにフォームをインスタンス化
-        userForm = UserForm(request.POST,instance=obj)
-        if userForm.is_valid():
-            userForm.save()
- 
-    #更新後、対象ユーザの情報を表示
-    # obj = Hinatazaka46.objects.get(id=id)
-    # context = {
-    #     'userinfoDetail': obj,
-    # }
- 
-    #detail.htmlへデータを渡す
-    return render(request, 'hinatazaka-list.html',context)
+
