7f468f3a125e447da0b90ba6aff807a1719253a4
sugiyama-mizuki <mk7gac4@gmail.com>
2022-06-21 04:27:49 +0900

    login 完成？

--- testPj/karaoke/forms.py
+++ testPj/karaoke/forms.py
@@ -13,3 +13,15 @@ class EnqueteForm(forms.Form):
         super().__init__(*args,**kwargs)
 
       self.fields['day'].widget.attrs['class']='form-control col-11'
+
+
+# ログイン
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['placeholder'] = field.label#全てのフォームの部品にplaceholderを定義して、入力フォームにフォーム名が表示されるように指定する
--- testPj/karaoke/views.py
+++ testPj/karaoke/views.py
@@ -11,3 +11,13 @@ class EnqueteView(generic.FormView):
     template_name = "karaoke-input.html"
     form_class = EnqueteForm #forms.pyで作ったクラス名
     success_url = reverse_lazy('information:enquete')
+
+# ログイン
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
+from  .forms import LoginForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class Login(LoginView):
+    template_name = 'karaoke-login.html'
+    form_class = LoginForm
