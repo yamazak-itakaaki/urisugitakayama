393ec41ed5396e84ca48fb727532933613a0caad
sugiyama-mizuki <mk7gac4@gmail.com>
2022-06-21 09:58:24 +0900

    新規登録ver1

--- testPj/karaoke/forms.py
+++ testPj/karaoke/forms.py
@@ -17,11 +17,24 @@ class EnqueteForm(forms.Form):
 
 # ログイン
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from .models import Kaiintouroku
 
-class LoginForm(AuthenticationForm):
+# フォームクラス作成
+class AccountForm(forms.ModelForm):
+    # パスワード入力：非表示対応
+    password = forms.CharField(widget=forms.PasswordInput(),label="パスワード")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['placeholder'] = field.label#全てのフォームの部品にplaceholderを定義して、入力フォームにフォーム名が表示されるように指定する
+    class Meta():
+        # ユーザー認証
+        model = User
+        # フィールド指定
+        fields = ('username','email','password')
+        # フィールド名指定
+        labels = {'username':"ユーザーID",'email':"メール"}
+
+class AddAccountForm(forms.ModelForm):
+    class Meta():
+        # モデルクラスを指定
+        model = Kaiintouroku
+        fields = ('miyoji','name','address',)
+        labels = {'miyoji':"苗字",'name':"名前",'address':"電話番号",}
